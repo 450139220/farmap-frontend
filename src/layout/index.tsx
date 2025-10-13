2b09807aa4cb0bbace65e1e2bc2054c8871fc3e8
Cham <450139220@qq.com>
2025-10-13 14:52:13 +0800

    refactor: rebuild with antd

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,61 +1,103 @@
-import { Outlet, NavLink, useNavigate } from "react-router";
+import { Layout as LayoutAntd, Menu, theme } from "antd";
+import { Outlet, NavLink, useNavigate, useLocation } from "react-router";
 import { routes } from "@/routes/routes";
 import style from "./index.module.css";
 import { useUser } from "@/store";
 
+const { Header, Sider, Content } = LayoutAntd;
+
 function Layout() {
-  const username = useUser((state) => state.username);
-  const navigate = useNavigate();
+    const username = useUser((state) => state.username);
+    const navigate = useNavigate();
 
-  // navigate to login page
-  const toLogin = () => {
-    navigate("/login");
-  };
+    // navigate to login page
+    const toLogin = () => {
+        navigate("/login");
+    };
 
-  return (
-    <>
-      <header>
-        <div className={style.logo}>
-          <img
-            className={style.logo__icon}
-            // src=""
-            alt="logo"
-          />
-          <span>FarMap</span>
-        </div>
-        <div className={style.links}>
-          {routes.map((route) => (
-            <NavLink
-              className={({ isActive }) => `${style.link} ${isActive ? style["link-active"] : ""}`}
-              key={route.handle.key}
-              to={route.path!}>
-              <span>&nbsp;{route.handle.name}&nbsp;</span>
-              <i className="ri-arrow-right-up-line"></i>
-            </NavLink>
-          ))}
-        </div>
-        <div className={style.login}>
-          {username ? (
-            <span>
-              您好，<span className={style["login-success"]}>{username}</span>！
-            </span>
-          ) : (
-            <span onClick={toLogin}>
-              请点此<span className={style["login-warning"]}>登陆</span>！
-            </span>
-          )}
-          <img
-            className={style.logo__icon}
-            // src=""
-            alt="avatar"
-          />
-        </div>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </>
-  );
+    // sider
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+    // router
+    const location = useLocation();
+
+    return (
+        <LayoutAntd className={style.layout__container}>
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0">
+                <div className={style.logo}>FarMap 农业数字地图</div>
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    selectedKeys={[location.pathname]}
+                    items={routes.map((route) => ({
+                        key: route.path!,
+                        label: (
+                            <NavLink to={route.path!}>
+                                <span>{route.handle.name}</span>
+                                <i className="ri-arrow-right-up-line"></i>
+                            </NavLink>
+                        ),
+                    }))}
+                />
+            </Sider>
+            <LayoutAntd>
+                <Header style={{ padding: 0, background: colorBgContainer }} />
+
+                <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+                    <div
+                        style={{
+                            padding: 24,
+                            textAlign: "center",
+                            background: colorBgContainer,
+                            borderRadius: borderRadiusLG,
+                        }}>
+                        <Outlet />
+                    </div>
+                </Content>
+                {/* <Footer style={{ textAlign: "center" }}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer> */}
+            </LayoutAntd>
+        </LayoutAntd>
+    );
+
+    return (
+        <>
+            <header>
+                <div className={style.logo}>
+                    <img
+                        className={style.logo__icon}
+                        // src=""
+                        alt="logo"
+                    />
+                    <span>FarMap</span>
+                </div>
+                <div className={style.links}>{}</div>
+                <div className={style.login}>
+                    {username ? (
+                        <span>
+                            您好，<span className={style["login-success"]}>{username}</span>！
+                        </span>
+                    ) : (
+                        <span onClick={toLogin}>
+                            请点此<span className={style["login-warning"]}>登陆</span>！
+                        </span>
+                    )}
+                    <img
+                        className={style.logo__icon}
+                        // src=""
+                        alt="avatar"
+                    />
+                </div>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </>
+    );
 }
 
 export default Layout;
