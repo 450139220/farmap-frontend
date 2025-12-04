7c01b94bf10692a67592e5c3c89abe930b75473c
CHAM <450139220@qq.com>
2025-12-04 17:00:43 +0800

    feat: create monitor list

---
+++ src/views/monitor/monitorList.ts
@@ -0,0 +1,31 @@
+export interface MonitorType {
+  serial: string;
+  name: string;
+}
+
+export const monitorList: MonitorType[] = [
+  { serial: "GC9054260", name: "廖荣华-球机" },
+  { serial: "GA1738420", name: "陈仁俊-球机" },
+  { serial: "GA1738425", name: "刘庆贵-球机" },
+  { serial: "GA1738438", name: "王祖保-球机" },
+  { serial: "GA1738413", name: "贺维清-球机" },
+  { serial: "GA1738435", name: "聂宗寿-球机" },
+  { serial: "GA1738401", name: "刘书建-球机" },
+  { serial: "GA3302963", name: "彭美春-球机" },
+  { serial: "GA1738422", name: "邹贤亮-球机" },
+  { serial: "GA1738434", name: "罗伟-球机" },
+  { serial: "GA3302944", name: "刘毅-球机" },
+  { serial: "GA1738431", name: "彭斌-球机" },
+  { serial: "GA1738418", name: "李军-球机" },
+  { serial: "GD1221224", name: "龚正第-球机" },
+  { serial: "GC5448241", name: "陈嗣春-球机" },
+  { serial: "GD1221262", name: "李洪-球机" },
+  { serial: "GA1738411", name: "李淑桂-球机" },
+  { serial: "GA1738437", name: "张怀-球机" },
+  { serial: "GA1738393", name: "向耀松-球机" },
+  { serial: "GA3302969", name: "翁发福-球机" },
+  { serial: "GA3302956", name: "陈勇-球机" },
+  { serial: "GA1738402", name: "覃传德-球机" },
+  { serial: "GA1738409", name: "刘绪海-球机" },
+  { serial: "GA1738433", name: "赖明星-球机" },
+];
